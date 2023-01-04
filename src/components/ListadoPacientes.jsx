4d975015b1bfcc64945e5e1643807eb400e42d3e
carlosYoko <carlosgibe@gmail.com>
2023-01-04 07:30:01 +0100

    primer commit

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,37 @@
+import Paciente from "./Paciente";
+
+const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll">
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">Pacientes</span>
+          </p>
+          {pacientes.map((paciente) => (
+            <Paciente
+              eliminarPaciente={eliminarPaciente}
+              key={paciente.id}
+              paciente={paciente}
+              setPaciente={setPaciente}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Agrega a tus pacientes {""}
+            <span className="text-indigo-600 font-bold">
+              y apareceran en este lugar
+            </span>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ListadoPacientes;
